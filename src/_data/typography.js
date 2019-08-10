6b54233d369de1eadc8525da7c5a1abb89ce397b
anyuzx <stefanshi1988@gmail.com>
2019-08-09 21:58:42 -0500

    move typography to include folder and use as filter

--- src/_data/typography.js
+++
@@ -1,8 +0,0 @@
-const Typography = require('typography')
-const TypographyBootstrap = require('typography-theme-bootstrap').default
-
-const typography = new Typography(TypographyBootstrap)
-
-module.exports = {
-  typography
-}
