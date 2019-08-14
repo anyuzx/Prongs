7b1c8a62d550467e4c8fd69c296bde4e4ef13845
anyuzx <stefanshi1988@gmail.com>
2019-08-14 15:32:05 -0500

    fixed to be compliant with the .tmp generated folder

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
-    './src/**/*.njk',
-    './dist/**/*.html'
+    './.tmp/**/*.html'
   ],
 
   // Include any special characters you're using in this regular expression
