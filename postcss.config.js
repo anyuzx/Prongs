c17440898640429e97279c14d1109d61c193ad7f
anyuzx <stefanshi1988@gmail.com>
2019-08-14 15:03:19 -0500

    use postcss for processing tailwincss and purgecss

---
+++ postcss.config.js
@@ -0,0 +1,23 @@
+// configuration for postcss
+// for more details, refer to https://tailwindcss.com/docs/controlling-file-size/#app
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: [
+    './src/**/*.njk',
+    './dist/**/*.html'
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
+  ]
+}
+
