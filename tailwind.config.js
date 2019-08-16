254550831bdbaf34f8286d34b66a37c395234825
anyuzx <stefanshi1988@gmail.com>
2019-08-16 12:02:52 -0500

    extend border width to solve the navigation to header problem (http://nicolasgallagher.com/jump-links-and-viewport-positioning/demo/\#method-D)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+      borderWidth: {
+        '20': '5rem',
+        '32': '8rem'
+      }
+    }
   },
   variants: {
     textDecoration: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
-    justifyContent: ['responsive']
+    justifyContent: ['responsive'],
+    borderWidth: ['responsive']
   },
   plugins: []
 }
