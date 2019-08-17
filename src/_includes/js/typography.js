e7fb1b65883dfa157d6d981479e6ef3d83b98b40
anyuzx <stefanshi1988@gmail.com>
2019-08-17 02:09:56 -0500

    override line-height

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -1,5 +1,6 @@
 const Typography = require('typography')
 const TypographyBootstrap = require('typography-theme-bootstrap').default
+TypographyBootstrap.baseLineHeight = 1.63
 
 const typography = new Typography(TypographyBootstrap)
 
