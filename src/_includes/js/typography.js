8a4f9159c3d72bcb71bf5c516b7cc0ff0e641641
anyuzx <stefanshi1988@gmail.com>
2019-08-17 21:49:15 -0500

    disable the normalize.css, tailwindcss provide normalize.css

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -3,6 +3,7 @@ const TypographyBootstrap = require('typography-theme-bootstrap').default
 TypographyBootstrap.baseLineHeight = 1.63
 
 const typography = new Typography(TypographyBootstrap)
+typography.options.includeNormalize = false
 
 module.exports = function () {
   return typography.toString()
