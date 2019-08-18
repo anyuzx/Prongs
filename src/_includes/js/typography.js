e65bd0002976318175a59b9a75d3094f529ea30e
anyuzx <stefanshi1988@gmail.com>
2019-08-17 22:43:29 -0500

    cleaner code

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -2,8 +2,7 @@ const Typography = require('typography')
 const TypographyBootstrap = require('typography-theme-bootstrap').default
 TypographyBootstrap.baseLineHeight = 1.63
 
-const typography = new Typography(TypographyBootstrap)
-typography.options.includeNormalize = false
+const typography = new Typography({ ...TypographyBootstrap, includeNormalize: false })
 
 module.exports = function () {
   return typography.toString()
