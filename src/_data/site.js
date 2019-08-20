6732db96bf56d176d7a57de9dd02144b8e9a094a
anyuzx <stefanshi1988@gmail.com>
2019-08-20 17:22:24 -0500

    fix url

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
   "title": "Guang Shi",
   "description": "Guang Shi's personal site. Learn about my research projects, read my blog posts and see my photos.",
-  "url": "https://guang-shi.com",
+  "url": "https://www.guangshi.io",
   "author": "Guang Shi",
   "email": "guang.shi.gs@gmail.com",
   "buildTime" : new Date()
