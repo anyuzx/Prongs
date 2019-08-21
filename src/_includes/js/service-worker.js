9b98993c76b5390bba66b4192c4bcace5b9c5183
anyuzx <stefanshi1988@gmail.com>
2019-08-21 16:02:24 -0500

    fix error Uncaught (in promise) TypeError: Request failed service worker

--- src/_includes/js/service-worker.js
+++ src/_includes/js/service-worker.js
@@ -8,14 +8,14 @@ const EXCLUDED_URLS = [
   'admin',
   '.netlify',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
-  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js'
+  'https://unpkg.com/netlify-cms@^2.9.7/dist/netlify-cms.js'
 ];
 
 // URLS that we want to be cached when the worker is installed
-const PRE_CACHE_URLS = ['/', '/fonts/lora-v13-latin-700.woff2'];
+const PRE_CACHE_URLS = ['/'];
 
 // You might want to bypass a certain host
-const IGNORED_HOSTS = ['localhost', 'unpkg.com', ];
+const IGNORED_HOSTS = ['localhost', 'unpkg.com'];
 
 /**
  * Takes an array of strings and puts them in a named cache store
