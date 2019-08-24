e37d2037d028d41894f91f5cd185fbeb21015fa8
anyuzx <stefanshi1988@gmail.com>
2019-08-24 01:11:07 -0500

    build a custom preview template

---
+++ src/admin/preview.js
@@ -0,0 +1,26 @@
+import mdRender from '../_includes/js/mdRender.js';
+
+const env = nunjucks.configure();
+
+env.addFilter('mdFilter', mdRender.render)
+
+const Preview = ({ entry, path, context }) => {
+  const data = context(entry.get('data').toJS());
+  const html = env.render(path, { ...data })
+  return <div dangerouslySetInnerHTML={{ __html: html }}/>
+}
+
+const Post = ({ entry }) => {
+  <Preview
+    entry = {entry}
+    path = "layouts/post.njk"
+    context={({ title, date, tags, body }) => ({
+      title,
+      date,
+      tags,
+      content: mdFilter(body || ''),
+    })}
+  />
+}
+
+CMS.registerPreviewTemplate('posts', Post);
