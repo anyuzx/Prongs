f4af3bb78a51a6fb62c83b580bb28b930c1e19bf
anyuzx <stefanshi1988@gmail.com>
2019-08-25 15:50:52 -0500

    debug: markdown-it-prism. problem with rollup bundle

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ const builtins = require('rollup-plugin-node-builtins');
 const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const json = require('rollup-plugin-json');
+const babel = require('rollup-plugin-babel');
 
 export default {
   input: 'src/admin/preview.js',
@@ -10,9 +11,14 @@ export default {
     format: 'esm',
   },
   plugins: [
-    builtins(),
-    nodeResolve(),
+    nodeResolve({
+      customResolveOptions: {
+        moduleDirectory: 'node_modules'
+      }
+    }),
     commonjs({ignore: ["conditional-runtime-dependency"]}),
+    builtins(),
     json(),
+    babel(),
   ]
 };
