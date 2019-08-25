273c03201675b346ed8db7bf846af9ea55676100
anyuzx <stefanshi1988@gmail.com>
2019-08-25 00:57:12 -0500

    debug:use dangerouslySetInnerHTML to render html generated by markdown-it

--- src/admin/preview.js
+++ src/admin/preview.js
@@ -6,12 +6,13 @@ const html = htm.bind(h); //'h' here is short hand for React.createElement
 var Post = createClass({
   render() {
     const entry = this.props.entry;
+    const bodyRendered = mdRender.render(entry.getIn(["data", "body"]));
 
     return html`
       <main>
         <article>
           <h1>${entry.getIn(["data", "title"], null)}</h1>
-          <div dangerouslySetInnerHTML={{__html: ${mdRender.render(entry.getIn(["data", "body"]))}}}/>
+          <div dangerouslySetInnerHTML=${{__html: bodyRendered}}></div>
           ${this.props.widgetFor("body")}
         </article>
       </main>
