b691ea98707d09c927ad741216cc3d16475ee20b
anyuzx <stefanshi1988@gmail.com>
2019-09-01 13:05:04 -0500

    add typeset.js for post-transform

---
+++ src/_includes/js/typeset.js
@@ -0,0 +1,11 @@
+const typeset = require('typeset');
+
+module.exports = (options) => {
+  return function applyTypeset(content, outputPath) {
+    if (outputPath && outputPath.endsWith('.html')) {
+      const result = typeset(content, options);
+      return result;
+    }
+    return content;
+  }
+}
