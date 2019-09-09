fcbdb8fd87877c8949419fca5575de287f805b90
anyuzx <stefanshi1988@gmail.com>
2019-09-08 22:16:28 -0500

    customize a few options

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -1,8 +1,12 @@
 const Typography = require('typography')
 const TypographyBootstrap = require('typography-theme-bootstrap').default
-TypographyBootstrap.baseLineHeight = 1.63
+//TypographyBootstrap.baseLineHeight = 1.63
+//TypographyBootstrap.overrideThemeStyles = (options, styles) => ({})
 
 const typography = new Typography({ ...TypographyBootstrap,
+                                    scaleRatio: 2.0,
+                                    baseLineHeight: 1.63,
+                                    headerWeight: 600,
                                     includeNormalize: false,
                                  })
 
