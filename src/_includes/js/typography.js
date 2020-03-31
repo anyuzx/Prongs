6d33c47d1556e39da426cffc0422ed98b12ddeb3
anyuzx <stefanshi1988@gmail.com>
2020-03-30 20:32:31 -0500

    tweak stype

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -4,8 +4,8 @@ const TypographyBootstrap = require('typography-theme-bootstrap').default
 //TypographyBootstrap.overrideThemeStyles = (options, styles) => ({})
 
 const typography = new Typography({ ...TypographyBootstrap,
-                                    scaleRatio: 2.0,
-                                    baseLineHeight: 1.63,
+                                    scaleRatio: 1.25,
+                                    baseLineHeight: 1.5,
                                     headerWeight: 600,
                                     includeNormalize: false,
                                     bodyFontFamily: ['IBM Plex Sans','-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif'],
