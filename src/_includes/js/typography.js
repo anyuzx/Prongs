c40bc1d57ff1cec36c41a34967098673983b3fe9
anyuzx <stefanshi1988@gmail.com>
2020-04-09 11:53:59 -0500

    change font to 'Inter'. Adjust font size, letter spacing and header font weight

--- src/_includes/js/typography.js
+++ src/_includes/js/typography.js
@@ -4,12 +4,13 @@ const TypographyBootstrap = require('typography-theme-bootstrap').default
 //TypographyBootstrap.overrideThemeStyles = (options, styles) => ({})
 
 const typography = new Typography({ ...TypographyBootstrap,
+                                    baseFontSize: 15px,
                                     scaleRatio: 1.25,
                                     baseLineHeight: 1.5,
-                                    headerWeight: 600,
+                                    headerWeight: 700,
                                     includeNormalize: false,
-                                    bodyFontFamily: ['IBM Plex Sans','-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif'],
-                                    headerFontFamily: ['IBM Plex Sans','-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif']
+                                    bodyFontFamily: ['Inter', '-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif'],
+                                    headerFontFamily: ['Inter', '-apple-system','BlinkMacSystemFont','Segoe UI','Roboto','Oxygen','Ubuntu','Cantarell','Fira Sans','Droid Sans','Helvetica Neue','sans-serif']
                                  })
 
 module.exports = function () {
