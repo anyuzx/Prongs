2d2848916e13da4387282813bc2d4159aac718e2
anyuzx <stefanshi1988@gmail.com>
2020-04-12 21:31:02 -0500

    change default font family class for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      'sans': ['"Inter var", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
+      'serif': ['Georgia, Cambria, "Times New Roman", Times, serif'],
+      'mono': ['"Fira Code", Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace']
+    },
     extend: {
       borderWidth: {
         '20': '5rem',
