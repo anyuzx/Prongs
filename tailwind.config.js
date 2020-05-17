646dc78e99eebed9c2501d308e4c3a83c431e423
anyuzx <stefanshi1988@gmail.com>
2020-05-17 02:51:34 -0500

    add tailwindcss typography plugin;clean-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,172 @@
 module.exports = {
   theme: {
-    fontFamily: {
-      'sans': ['system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'],
-      'serif': ['Georgia, Cambria, "Times New Roman", Times, serif'],
-      'mono': ['"IBM Plex Mono", "Fira Code", Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace'],
-      'system-sans': ['system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"']
+    fontSize: {
+      xs: ['12px', '22px'],
+      sm: ['14px', '25px'],
+      base: ['16px', '28px'],
+      lg: ['20px', '34px'],
+      xl: ['26px', '43px'],
+      '2xl': ['33px', '53px'],
+      '3xl': ['42px', '66px']
     },
+    textIndent: { // defaults to {}
+      '1': '0.25rem',
+      '2': '0.5rem',
+    },
+    textShadow: { // defaults to {}
+      'default': '0 2px 5px rgba(0, 0, 0, 0.5)',
+      'lg': '0 2px 10px rgba(0, 0, 0, 0.5)',
+    },
+    textDecorationStyle: { // defaults to these values
+      'solid': 'solid',
+      'double': 'double',
+      'dotted': 'dotted',
+      'dashed': 'dashed',
+      'wavy': 'wavy',
+    },
+    textDecorationColor: { // defaults to theme => theme('colors')
+      'red': '#f00',
+      'green': '#0f0',
+      'blue': '#00f',
+    },
+    fontVariantCaps: { // defaults to these values
+      'normal': 'normal',
+      'small': 'small-caps',
+      'all-small': 'all-small-caps',
+      'petite': 'petite-caps',
+      'unicase': 'unicase',
+      'titling': 'titling-caps',
+    },
+    fontVariantNumeric: { // defaults to these values
+      'normal': 'normal',
+      'ordinal': 'ordinal',
+      'slashed-zero': 'slashed-zero',
+      'lining': 'lining-nums',
+      'oldstyle': 'oldstyle-nums',
+      'proportional': 'proportional-nums',
+      'tabular': 'tabular-nums',
+      'diagonal-fractions': 'diagonal-fractions',
+      'stacked-fractions': 'stacked-fractions',
+    },
+    fontVariantLigatures: { // defaults to these values
+      'normal': 'normal',
+      'none': 'none',
+      'common': 'common-ligatures',
+      'no-common': 'no-common-ligatures',
+      'discretionary': 'discretionary-ligatures',
+      'no-discretionary': 'no-discretionary-ligatures',
+      'historical': 'historical-ligatures',
+      'no-historical': 'no-historical-ligatures',
+      'contextual': 'contextual',
+      'no-contextual': 'no-contextual',
+    },
+    textRendering: { // defaults to these values
+      'rendering-auto': 'auto',
+      'optimize-legibility': 'optimizeLegibility',
+      'optimize-speed': 'optimizeSpeed',
+      'geometric-precision': 'geometricPrecision'
+    },
+    textStyles: theme => ({ // defaults to {}
+      heading: {
+        output: false, // this means there won't be a "heading" component in the CSS, but it can be extended
+        fontWeight: theme('fontWeight.bold'),
+        lineHeight: theme('lineHeight.tight'),
+      },
+      h1: {
+        extends: 'heading', // this means all the styles in "heading" will be copied here; "extends" can also be an array to extend multiple text styles
+        fontSize: theme('fontSize.5xl'),
+        '@screen sm': {
+          fontSize: theme('fontSize.6xl'),
+        },
+      },
+      h2: {
+        extends: 'heading',
+        fontSize: theme('fontSize.4xl'),
+        '@screen sm': {
+          fontSize: theme('fontSize.5xl'),
+        },
+      },
+      h3: {
+        extends: 'heading',
+        fontSize: theme('fontSize.4xl'),
+      },
+      h4: {
+        extends: 'heading',
+        fontSize: theme('fontSize.3xl'),
+      },
+      h5: {
+        extends: 'heading',
+        fontSize: theme('fontSize.2xl'),
+      },
+      h6: {
+        extends: 'heading',
+        fontSize: theme('fontSize.xl'),
+      },
+      link: {
+        fontWeight: theme('fontWeight.bold'),
+        color: theme('colors.blue.400'),
+        '&:hover': {
+          color: theme('colors.blue.600'),
+          textDecoration: 'underline',
+        },
+      },
+      richText: {
+        fontWeight: theme('fontWeight.normal'),
+        fontSize: theme('fontSize.base'),
+        lineHeight: theme('lineHeight.relaxed'),
+        '> * + *': {
+          marginTop: '1em',
+        },
+        'h1': {
+          extends: 'h1',
+        },
+        'h2': {
+          extends: 'h2',
+        },
+        'h3': {
+          extends: 'h3',
+        },
+        'h4': {
+          extends: 'h4',
+        },
+        'h5': {
+          extends: 'h5',
+        },
+        'h6': {
+          extends: 'h6',
+        },
+        'ul': {
+          listStyleType: 'disc',
+        },
+        'ol': {
+          listStyleType: 'decimal',
+        },
+        'a': {
+          extends: 'link',
+        },
+        'b, strong': {
+          fontWeight: theme('fontWeight.bold'),
+        },
+        'i, em': {
+          fontStyle: 'italic',
+        },
+      },
+    }),
     extend: {
       borderWidth: {
         '16': '4rem',
         '20': '5rem',
         '24': '6rem',
         '32': '8rem'
+      },
+      colors: {
+        'darkblue': '#17184b',
+        'lightgray': '#f0f0f4',
+        'lightyellow': '#fffbf0',
+        'lightblue': '#edf2f7'
+      },
+      fontFamily: {
+      'mono': ['"IBM Plex Mono", SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace']
       }
     }
   },
@@ -20,9 +175,28 @@ module.exports = {
     borderWidth: ['responsive'],
     backgroundColor: ['responsive', 'hover', 'focus', 'active', 'group-hover', 'dark', 'dark-hover', 'dark-group-hover', 'dark-even', 'dark-odd'],
     borderColor: ['responsive', 'hover', 'focus', 'active', 'group-hover','dark', 'dark-focus', 'dark-focus-within'],
-    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover','dark', 'dark-hover', 'dark-active', 'dark-placeholder']
+    textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover','dark', 'dark-hover', 'dark-active', 'dark-placeholder'],
+    textIndent: ['responsive'],
+    textShadow: ['responsive'],
+    textDecorationStyle: ['responsive'],
+    textDecorationColor: ['responsive'],
+    ellipsis: ['responsive'],
+    hyphens: ['responsive'],
+    kerning: ['responsive'],
+    textUnset: ['responsive'],
+    fontVariantCaps: ['responsive'],
+    fontVariantNumeric: ['responsive'],
+    fontVariantLigatures: ['responsive'],
+    textRendering: ['responsive'],
   },
   plugins: [
-    require('tailwindcss-dark-mode')()
+    require('tailwindcss-dark-mode')(),
+    require('tailwindcss-typography')({
+      ellipsis: true,
+      hyphens: true,
+      kerning: true,
+      textUnset: true,
+      componentPrefix: 'c-'
+    })
   ]
 }
