68c0b1adc65ecaeb6f252e8be375b7df42d02cff
anyuzx <stefanshi1988@gmail.com>
2020-05-17 02:48:44 -0500

    formatting

--- criticalCSS.js
+++ criticalCSS.js
@@ -1,7 +1,7 @@
 var critical = require('critical')
 var fg = require('fast-glob')
 
-var htmlSources = fg.sync("dist/**/*.html")
+var htmlSources = fg.sync('dist/**/*.html')
 
 function startNewJob () {
   const source = htmlSources.pop()
@@ -15,22 +15,11 @@ function startNewJob () {
       html: source
     }
   })
-  .then(()=>{
-    console.log("inline critical CSS for "+source+" succeed!")
-    return startNewJob()
-  })
+    .then(() => {
+      console.log('inline critical CSS for "+source+" succeed!')
+      return startNewJob()
+    })
 }
-/*
-htmlSources.forEach(function(item){
-  promises.push(critical.generate({
-    inline: true,
-    src: item,
-    target: {
-      html: item
-    }
-  }))
-})
-*/
 
 Promise.all([
   startNewJob(),
@@ -39,6 +28,6 @@ Promise.all([
   startNewJob(),
   startNewJob()
 ])
-.then(()=>{
-  console.log('all done!')
-})
+  .then(() => {
+    console.log('all done!')
+  })
