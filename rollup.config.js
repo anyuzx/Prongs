9014ab3b43a68be0fcfb08370daff277042258fb
anyuzx <stefanshi1988@gmail.com>
2020-05-17 02:49:24 -0500

    formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-const builtins = require('rollup-plugin-node-builtins');
-const commonjs = require('rollup-plugin-commonjs');
-const nodeResolve = require('rollup-plugin-node-resolve');
-const json = require('rollup-plugin-json');
-const babel = require('rollup-plugin-babel');
+const builtins = require('rollup-plugin-node-builtins')
+const commonjs = require('rollup-plugin-commonjs')
+const nodeResolve = require('rollup-plugin-node-resolve')
+const json = require('rollup-plugin-json')
+const babel = require('rollup-plugin-babel')
 
 export default {
   input: 'src/admin/preview.js',
   output: {
     file: 'dist/admin/preview.js',
-    format: 'esm',
+    format: 'esm'
   },
   plugins: [
-    nodeResolve({browser:true}),
-    commonjs({ignore: ["conditional-runtime-dependency"]}),
+    nodeResolve({ browser: true }),
+    commonjs({ ignore: ['conditional-runtime-dependency'] }),
     builtins(),
     json(),
     babel({
-      "plugins": [
-        ["prismjs", {
-          "languages": ["javascript", "css", "markup", "python", "clike"]
+      plugins: [
+        ['prismjs', {
+          languages: ['javascript', 'css', 'markup', 'python', 'clike']
         }]
       ]
     })
   ]
-};
+}
