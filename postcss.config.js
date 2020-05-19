8eef4216a97f64d9a6948db6f4bb36f4d876e3b7
anyuzx <stefanshi1988@gmail.com>
2020-05-18 19:46:04 -0500

    use postcss before 11ty build. thus do not use purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,28 +1,10 @@
 // configuration for postcss
-// for more details, refer to https://tailwindcss.com/docs/controlling-file-size/#app
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: [
-    './dist/**/*.html'
-  ],
-
-  whitelist: [
-    'mode-dark'
-  ],
-
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-})
-
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano')({
       preset: 'default'
-    }),
-    ...process.env.ELEVENTY_ENV === 'production'
-      ? [purgecss]
-      : []
+    })
   ]
 }
