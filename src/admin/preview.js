42b1c3fad666708db007b1d41fa638617ec6d631
anyuzx <stefanshi1988@gmail.com>
2020-05-19 15:04:56 -0500

    directly use mdRender from _includes/js/mdRender.js;import htm from node_module

--- src/admin/preview.js
+++ src/admin/preview.js
@@ -1,27 +1,29 @@
-import mdRender from './util.js';
-//https://github.com/developit/htm
-import htm from 'https://unpkg.com/htm?module';
-//'h' here is short hand for React.createElement
-const html = htm.bind(h);
+// https://github.com/developit/htm
+// import htm from 'https://unpkg.com/htm?module'
+import htm from 'htm'
+const mdRender = require('../_includes/js/mdRender.js')
+//import mdRender from './util.js';
+// 'h' here is short hand for React.createElement
+const html = htm.bind(h)
 
 var Post = createClass({
-  render() {
-    const entry = this.props.entry;
-    const title = entry.getIn(["data", "title"], null);
-    const body = entry.getIn(["data", "body"], null);
-    const bodyRendered = mdRender.render(body || '');
+  render () {
+    const entry = this.props.entry
+    const title = entry.getIn(['data', 'title'], null)
+    const body = entry.getIn(['data', 'body'], null)
+    const bodyRendered = mdRender.render(body || '')
 
     return html`
     <body>
       <main>
         <article>
           <h1>${title}</h1>
-          <div dangerouslySetInnerHTML=${{__html: bodyRendered}}></div>
+          <div dangerouslySetInnerHTML=${{ __html: bodyRendered }}></div>
         </article>
       </main>
     </body>
-    `;
+    `
   }
-});
+})
 
-CMS.registerPreviewTemplate('posts', Post);
+CMS.registerPreviewTemplate('posts', Post)
