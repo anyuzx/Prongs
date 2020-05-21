57886fc35c4dde9dc851b8fc124fdbb28e5339d1
anyuzx <stefanshi1988@gmail.com>
2020-05-21 17:38:33 -0500

    add purgecss config file; run purgecss at the end of each build; generate a minified (purged) version of main.css

---
+++ purgecss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['dist/**/*.html'],
+    css: ['dist/_includes/css/main.css'],
+    output: 'dist/_includes/css/main.min.css',
+    // This is the function used to extract class names from your templates
+    defaultExtractor: content => {
+        // Capture as liberally as possible, including things like `h-(screen-1.5)`
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
+
+        // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
+
+        return broadMatches.concat(innerMatches)
+    }
+}
