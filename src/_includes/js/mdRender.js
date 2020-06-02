b3548c6a364fb022522661da1bffbdfd72a0c324
anyuzx <stefanshi1988@gmail.com>
2020-06-01 21:50:47 -0500

    add anchorButton container; add custom highlightjs function

--- src/_includes/js/mdRender.js
+++ src/_includes/js/mdRender.js
@@ -15,25 +15,65 @@ const markdownItKbd = require('markdown-it-kbd')
 const markdownItTOC = require('markdown-it-toc-done-right')
 
 const hljs = require('highlight.js')
+// the following part is from markdown-it-highlight plugin
+const maybe = f => {
+  try {
+    return f()
+  } catch (e) {
+    return false
+  }
+}
+
+// Allow registration of other languages.
+const registerLangs = (register) => register &&
+  Object.entries(register).map(([lang, pack]) => { hljs.registerLanguage(lang, pack) })
+
+// Highlight with given language.
+const highlight = (code, lang) =>
+  maybe(() => hljs.highlight(lang || 'plaintext', code, true).value) || ''
+
+// Highlight with given language or automatically.
+const highlightAuto = (code, lang) =>
+  lang
+    ? highlight(code, lang)
+    : maybe(() => hljs.highlightAuto(code).value) || ''
+
+// Wrap a render function to add `hljs` class to code blocks.
+const wrap = render =>
+  function (...args) {
+    return render.apply(this, args)
+      .replace('<pre><code class="', '<pre class="hljs"><code class="hljs ')
+      .replace('<code>', '<code class="hljs">')
+  }
+
+const highlightjs = (md, opts) => {
+  opts = Object.assign({}, highlightjs.defaults, opts)
+  registerLangs(opts.register)
+
+  md.options.highlight = opts.auto ? highlightAuto : highlight
+  md.renderer.rules.fence = wrap(md.renderer.rules.fence)
+
+  if (opts.code) {
+    md.renderer.rules.code_block = wrap(md.renderer.rules.code_block)
+  }
+}
+
+highlightjs.defaults = {
+  auto: true,
+  code: true
+}
 
 // customize markdown-it
 const options = {
   html: true,
   typographer: true,
-  linkify: true,
-  highlight: function (str, lang) {
-    if (lang && hljs.getLanguage(lang)) {
-      try {
-        return '<pre class="hljs"><code>' + hljs.highlight(lang, str, true).value + '</code></pre>'
-      } catch (_) {}
-    }
-    return '<pre class="hljs"><code>' + markdownIt.utils.escapeHtml(str) + '</code></pre>'
-  }
+  linkify: true
 }
 
 var customMarkdownIt = new markdownIt(options)
 
 customMarkdownIt
+  .use(highlightjs)
   .use(markdownItKatex, { throwOnError: false, errorColor: ' #cc0000' })
   .use(markdownItFootnote)
   .use(markdownImplicitFigure, {
@@ -65,6 +105,22 @@ customMarkdownIt
       }
     }
   })
+  .use(require('markdown-it-container'), 'anchorButton', {
+
+    validate: function (params) {
+      return params.trim().match(/^anchorButton\s+(.*)$/)
+    },
+
+    render: function (tokens, idx) {
+      var m = tokens[idx].info.trim().match(/^anchorButton\s+(.*)$/)
+
+      if (tokens[idx].nesting === 1) {
+        return `<button class="l-button" onClick="location.href='${customMarkdownIt.utils.escapeHtml(m[1])}'">`
+      } else {
+        return '</button>\n'
+      }
+    }
+  })
   .use(require('markdown-it-container'), 'linkButton', {
 
     validate: function (params) {
@@ -75,13 +131,9 @@ customMarkdownIt
       var m = tokens[idx].info.trim().match(/^linkButton\s+(.*)$/)
 
       if (tokens[idx].nesting === 1) {
-        if (m[1] === undefined) {
-          return "<button class='linkButton'>"
-        } else {
-          return `<button class="linkButton" onClick="location.href='${customMarkdownIt.utils.escapeHtml(m[1])}'">`
-        }
+        return `<a class="l-button" href='${customMarkdownIt.utils.escapeHtml(m[1])}'>`
       } else {
-        return '</button>\n'
+        return '</a>\n'
       }
     }
   })
