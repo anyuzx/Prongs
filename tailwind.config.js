91d1836bf0013a9bd082d3878a2661df72255bd8
anyuzx <stefanshi1988@gmail.com>
2020-06-01 21:44:49 -0500

    delete unnecessary parts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,8 @@
 module.exports = {
   theme: {
-    fontSize: {
-      xs: ['12px', '22px'],
-      sm: ['14px', '25px'],
-      base: ['16px', '28px'],
-      lg: ['20px', '34px'],
-      xl: ['26px', '43px'],
-      '2xl': ['33px', '53px'],
-      '3xl': ['42px', '66px']
-    },
     textIndent: { // defaults to {}
       '1': '0.25rem',
       '2': '0.5rem',
-    },
-    textShadow: { // defaults to {}
-      'default': '0 2px 5px rgba(0, 0, 0, 0.5)',
-      'lg': '0 2px 10px rgba(0, 0, 0, 0.5)',
     },
     textDecorationStyle: { // defaults to these values
       'solid': 'solid',
@@ -66,93 +53,10 @@ module.exports = {
       'optimize-speed': 'optimizeSpeed',
       'geometric-precision': 'geometricPrecision'
     },
-    textStyles: theme => ({ // defaults to {}
-      heading: {
-        output: false, // this means there won't be a "heading" component in the CSS, but it can be extended
-        fontWeight: theme('fontWeight.bold'),
-        lineHeight: theme('lineHeight.tight'),
-      },
-      h1: {
-        extends: 'heading', // this means all the styles in "heading" will be copied here; "extends" can also be an array to extend multiple text styles
-        fontSize: theme('fontSize.5xl'),
-        '@screen sm': {
-          fontSize: theme('fontSize.6xl'),
-        },
-      },
-      h2: {
-        extends: 'heading',
-        fontSize: theme('fontSize.4xl'),
-        '@screen sm': {
-          fontSize: theme('fontSize.5xl'),
-        },
-      },
-      h3: {
-        extends: 'heading',
-        fontSize: theme('fontSize.4xl'),
-      },
-      h4: {
-        extends: 'heading',
-        fontSize: theme('fontSize.3xl'),
-      },
-      h5: {
-        extends: 'heading',
-        fontSize: theme('fontSize.2xl'),
-      },
-      h6: {
-        extends: 'heading',
-        fontSize: theme('fontSize.xl'),
-      },
-      link: {
-        fontWeight: theme('fontWeight.bold'),
-        color: theme('colors.blue.400'),
-        '&:hover': {
-          color: theme('colors.blue.600'),
-          textDecoration: 'underline',
-        },
-      },
-      richText: {
-        fontWeight: theme('fontWeight.normal'),
-        fontSize: theme('fontSize.base'),
-        lineHeight: theme('lineHeight.relaxed'),
-        '> * + *': {
-          marginTop: '1em',
-        },
-        'h1': {
-          extends: 'h1',
-        },
-        'h2': {
-          extends: 'h2',
-        },
-        'h3': {
-          extends: 'h3',
-        },
-        'h4': {
-          extends: 'h4',
-        },
-        'h5': {
-          extends: 'h5',
-        },
-        'h6': {
-          extends: 'h6',
-        },
-        'ul': {
-          listStyleType: 'disc',
-        },
-        'ol': {
-          listStyleType: 'decimal',
-        },
-        'a': {
-          extends: 'link',
-        },
-        'b, strong': {
-          fontWeight: theme('fontWeight.bold'),
-        },
-        'i, em': {
-          fontStyle: 'italic',
-        },
-      },
-    }),
     extend: {
+      margin: {
+        '7': '1.75rem'
+      },
       borderWidth: {
         '16': '4rem',
         '20': '5rem',
