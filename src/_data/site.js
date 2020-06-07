2673b07af64f44d2e8985c632c97ffea83646a72
anyuzx <stefanshi1988@gmail.com>
2020-06-07 11:20:46 -0500

    update

--- src/_data/site.js
+++ src/_data/site.js
@@ -5,14 +5,14 @@
 //  site.TITLE, site.DESCRIPTION, etc
 
 module.exports = {
-  TITLE: 'guangshi.io', // site title, this is used across the site. For instance the name appeared in the browser tab will be [page_name] | [site_title]
+  TITLE: 'www.guangshi.io', // site title, this is used across the site. For instance the name appeared in the browser tab will be [page_name] | [site_title]
   DESCRIPTION: "Guang Shi's personal site. Research, blog and photography", // site description, used in meta head for SEO purpose
   URL: 'https://www.guangshi.io', // absolute url for the site
   BUILD_TIME: new Date(), // build time for the site
   //'SOURCE_CODE': 'https://github.com/anyuzx/Prongs', // the github repo of the source code
   ENV: process.env.ELEVENTY_ENV, // environment variable can be accessed in templates https://www.11ty.io/docs/data-js/#example%3A-exposing-environment-variables
   FEED: {
-    PATH: 'feed.xml', // name of the RSS feed file. The file should be in the root of the site
+    PATH: '/feed.xml', // name of the RSS feed file. The file should be in the root of the site
     TITLE: 'Blog posts feed, by Guang Shi', // title for the RSS feed
   },
   //'GOOGLE_ANALYTICS': 'UA-139423558-3',
