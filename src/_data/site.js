94f5860e2651e3c1831dd5686ca628753b1ccbc3
anyuzx <stefanshi1988@gmail.com>
2020-06-13 00:49:15 -0500

    update personal short bio

--- src/_data/site.js
+++ src/_data/site.js
@@ -23,7 +23,7 @@ module.exports = {
     INSTAGRAM: 'guang_shi_stefan', // author's instagram
     TWITTER: 'anyuzx', // author's twitter handler
     GOOGLE_SCHOLAR: 'https://scholar.google.com/citations?user=JMf5dv8AAAAJ&hl=en', // google scholar profile
-    SHORT_BIO: 'I am a biophysicist trained in using physics-based computational and theoretical method to study the structure of dynamics of interphase chromosomes',
+    SHORT_BIO: 'I am a biophysicist trained in using physics-based computational and theoretical method to study interphase chromosome organization',
     TITLE: 'Postdoctoral Researcher',
     SECOND_TITLE: 'Thirumalai Lab @ UT Austin',
     PHOTO: '/assets/images/my-photo.jpg',
